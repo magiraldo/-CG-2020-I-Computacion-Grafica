8bc8311afca0f3ccd2fd7c3dab2cd41bfb188607
magiraldo <maria.giraldo@ucsp.edu.pe>
2020-04-07 04:11:30 -0500

    Prueba basica con librerias

---
+++ Prueba/Prueba1/main.cpp
@@ -0,0 +1,17 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    std::cout << "Hello World";
+    return 0;
+}
